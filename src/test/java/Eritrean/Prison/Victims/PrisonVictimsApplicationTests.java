eaa2f02ca16da86230dc0d65632ca280108b77d3
hebronMelles <147551219+hebronMelles@users.noreply.github.com>
2025-03-05 18:09:00 -0800

    Initial commit - Adding IntelliJ project

---
+++ src/test/java/Eritrean/Prison/Victims/PrisonVictimsApplicationTests.java
@@ -0,0 +1,13 @@
+package Eritrean.Prison.Victims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrisonVictimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
