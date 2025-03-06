5288d176d33ef305fb73df020a7fd3ecf0a5ce88
hebronMelles <147551219+hebronMelles@users.noreply.github.com>
2025-03-05 20:37:54 -0800

    added cognito user pool for sign up and log in

---
+++ src/main/java/Eritrean/Prison/Victims/Security/CognitoLogoutHandler.java
@@ -0,0 +1,46 @@
+package Eritrean.Prison.Victims.Security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Cognito has a custom logout url.
+ * See more information <a href="https://docs.aws.amazon.com/cognito/latest/developerguide/logout-endpoint.html">here</a>.
+ */
+public class CognitoLogoutHandler extends SimpleUrlLogoutSuccessHandler {
+
+    /**
+     * The domain of your user pool.
+     */
+    private String domain = "https://<user pool domain>";
+
+    /**
+     * An allowed callback URL.
+     */
+    private String logoutRedirectUrl = "<logout uri>";
+
+    /**
+     * The ID of your User Pool Client.
+     */
+    private String userPoolClientId = "1truinrsmh6jhu1e6fqemu6hh3";
+
+    /**
+     * Here, we must implement the new logout URL request. We define what URL to send our request to, and set out client_id and logout_uri parameters.
+     */
+    @Override
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        return UriComponentsBuilder
+                .fromUri(URI.create(domain + "/logout"))
+                .queryParam("client_id", userPoolClientId)
+                .queryParam("logout_uri", logoutRedirectUrl)
+                .encode(StandardCharsets.UTF_8)
+                .build()
+                .toUriString();
+    }
+}
